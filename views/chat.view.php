d04d0dca791a0bc0434a9196eda2141c7abd4908
Moussa <moussa76b@gmail.com>
2021-12-05 17:55:33 +0100

    mini_chat

--- views/chat.view.php
+++ views/chat.view.php
@@ -5,19 +5,24 @@ ob_start() ?>
 
 <div>
     <form action="<?= URL ?>chat" method="POST">
-            <label for="user">User : </label><input type="text" name='user' placeholder='Pseudo'>
-            <label for="message">Message : </label><input type="text" name='message' placeholder='écrire votre message ..'>
-            <input type="submit" name="insertchat" value='Send'>
+        <label for="user">User : </label><input type="text" name='user' placeholder='Pseudo'>
+        <label for="message">Message : </label><input type="text" name='message' placeholder='écrire votre message ..'>
+        <input type="submit" name="insertchat" value='Send'>
     </form>
 </div>
-<div>
-    <table>
-        <tr>
-            <td><?= $chats[0]->getUser();?></td>
-            <td><?= $chats[0]->getMessage();?></td>
-        </tr>
-    </table>
-</div>
+
+<?php
+for ($i = 0; $i < count($chats); $i++) : ?>
+    <div>
+        <table>
+            <tr>
+                <td class="align-middle"><?= $chats[$i]->getId(); ?></td>
+                <td class="align-middle"><?= $chats[$i]->getUser(); ?></td>
+                <td class="align-middle"><?= $chats[$i]->getMessage(); ?></td>
+            </tr>
+        </table>
+    </div>
+<?php endfor; ?>
 
 <?php
 $content = ob_get_clean();
